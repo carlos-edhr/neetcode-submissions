410a353f438e9a0c953ecbd61e3d45db971fbd82
carlos-ehr <eduardoh.rojas@protonmail.com>
2026-08-11 12:35:12 -0700

    Add: remove-duplicates-from-sorted-array - submission-7

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #'write' points to the position where the next unique element
+        #will be placed
+        write = 1
+        # 'read' scans the array starting from the second element
+        for read in range(1, len(nums)):
+            # if current element is different from previous one
+            # it is a new unique element
+            if nums[read] != nums[read - 1 ]:
+                nums[write] = nums[read]
+                write += 1
+        return write
